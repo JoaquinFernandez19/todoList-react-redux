ef3939ac66bb201920e63fd4972871ef726b8b86
Joaquin Fernandez <copako12@gmail.com>
2020-04-10 21:22:34 -0300

    Updates

---
+++ precache-manifest.a00552817665baf83f6c9184682a024a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd4c71d4364dd0c0d98b26097cf2a60",
+    "url": "/todoList-react-redux/index.html"
+  },
+  {
+    "revision": "8c1012051d162a844490",
+    "url": "/todoList-react-redux/static/js/2.b929ddc0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todoList-react-redux/static/js/2.b929ddc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c85bcac7e0b669eeb601",
+    "url": "/todoList-react-redux/static/js/main.283c5efe.chunk.js"
+  },
+  {
+    "revision": "610949eedce8d74ec0fc",
+    "url": "/todoList-react-redux/static/js/runtime-main.6030163d.js"
+  }
+]);
